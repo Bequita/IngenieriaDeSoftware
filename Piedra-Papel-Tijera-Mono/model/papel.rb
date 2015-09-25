947d2bd0c6f24d1b9256fcd8ab47b6ed1baad842
Bequita <hernan.beca@gmail.com>
2015-09-24 23:40:38 -0300

    Agregados tests del elemento papel

--- Piedra-Papel-Tijera-Mono/model/papel.rb
+++ Piedra-Papel-Tijera-Mono/model/papel.rb
@@ -1,2 +1,11 @@
-class Papel
+require_relative '../model/elementos'
+
+class Papel < Elementos
+
+  def resultado(elem1)
+    hs = {'Piedra' => 'Gana', 'Tijera' => 'Pierde', 'Mono' => 'Pierde', 'Papel' => 'Empata'}
+
+    hs[elem1.class.name]
+  end
+
 end
---
+++ Piedra-Papel-Tijera-Mono/spec/papel_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require_relative '../model/piedra'
+require_relative '../model/tijera'
+require_relative '../model/papel'
+require_relative '../model/mono'
+
+describe 'papel' do
+
+  let(:piedra) {Piedra.new}
+  let(:tijera) {Tijera.new}
+  let(:papel)  {Papel.new}
+  let(:mono)   {Mono.new}
+
+  it 'debe ganar cuando juega contra piedra' do
+    expect(papel.contra(piedra)).to eq 'Gana'
+  end
+
+  it 'debe perder cuando juega contra tijera' do
+    expect(papel.contra(tijera)).to eq 'Pierde'
+  end
+
+  it 'debe perder cuando juega contra mono' do
+    expect(papel.contra(mono)).to eq 'Pierde'
+  end
+
+  it 'debe empatar cuando juega contra papel' do
+    expect(papel.contra(papel)).to eq 'Empata'
+  end
+
+end
