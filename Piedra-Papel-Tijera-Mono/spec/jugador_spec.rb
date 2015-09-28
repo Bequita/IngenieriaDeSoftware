76a76f6415d240db0124cb307428394c76a09781
Bequita <hernan.beca@gmail.com>
2015-09-28 01:40:14 -0300

    Reparado tests jugador_spec

--- Piedra-Papel-Tijera-Mono/spec/jugador_spec.rb
+++ Piedra-Papel-Tijera-Mono/spec/jugador_spec.rb
@@ -3,8 +3,8 @@ require_relative '../model/jugador'
 
 describe 'jugador' do
 
-  jugador1 = Jugador.new
-  jugador2 = Jugador.new
+  jugador1 = Jugador.new('Jugador1')
+  jugador2 = Jugador.new('Jugador2')
 
   it 'el nombre del primer jugador deberia ser Jugador1' do
     expect(jugador1.nombreJugador).to eq 'Jugador1'
