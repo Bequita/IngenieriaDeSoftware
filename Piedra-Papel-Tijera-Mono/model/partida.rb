7025370cf09a5e7f80f5538e0e42a972e63f3b67
Bequita <hernan.beca@gmail.com>
2015-09-28 02:41:20 -0300

    Agregado test partida: la primer ronda no puede volver a jugarse

--- Piedra-Papel-Tijera-Mono/model/partida.rb
+++ Piedra-Papel-Tijera-Mono/model/partida.rb
@@ -1,17 +1,23 @@
 class Partida
 
-  attr_reader :jugador1, :jugador2, :ronda
+  attr_reader :jugador1, :jugador2, :ronda, :rondaActual
 
   def initialize(jug1, jug2, ron)
     @jugador1 = jug1
     @jugador2 = jug2
     @ronda = ron
+    @rondaActual = 1
   end
 
   def jugar_primera_ronda()
+    if @rondaActual.eql? 1
     resultado = ronda.jugar(jugador1, jugador2)
 
+    @rondaActual = 2
     diccionario_de_rondas(resultado, "primera")
+    else
+      raise 'La primera ronda ya fue jugada'
+    end
   end
 
   def diccionario_de_rondas(arg1, numero_de_ronda)
--- Piedra-Papel-Tijera-Mono/spec/partida_spec.rb
+++ Piedra-Papel-Tijera-Mono/spec/partida_spec.rb
@@ -9,21 +9,30 @@ require_relative'../model/mono'
 
 describe 'partida' do
 
-  let(:jug1) {Jugador.new('Jugador1')}
-  let(:jug2) {Jugador.new('Jugador2')}
-
   let(:piedra) {Piedra.new}
   let(:tijera) {Tijera.new}
   let(:papel) {Papel.new}
   let(:mono) {Mono.new}
 
-  let(:ronda) {Ronda.new}
-  let(:partida) {Partida.new(jug1, jug2, ronda)}
+  jug1 = Jugador.new('Jugador1')
+  jug2 = Jugador.new('Jugador2')
+  ronda = Ronda.new
+  partida = Partida.new(jug1, jug2, ronda)
 
-  it 'Jugador1 deberia ganar la primer ronda' do
-    jug1.elige_elemento(piedra)
-    jug2.elige_elemento(tijera)
-    expect(partida.jugar_primera_ronda).to eq 'Jugador1 gana la primera ronda'
+  context 'primera ronda' do
+
+    it 'Jugador1 deberia ganar la primer ronda' do
+      jug1.elige_elemento(piedra)
+      jug2.elige_elemento(tijera)
+      expect(partida.jugar_primera_ronda).to eq 'Jugador1 gana la primera ronda'
+    end
+
+    it 'no se tendria que poder volver a jugar la primer ronda' do
+      jug1.elige_elemento(piedra)
+      jug2.elige_elemento(tijera)
+      expect{partida.jugar_primera_ronda}.to raise_error('La primera ronda ya fue jugada')
+    end
+
   end
 
 end
