60cd0fc5cf17cc7c724bafe8745c71b7d0f063b0
Bequita <hernan.beca@gmail.com>
2015-10-19 23:01:42 -0300

    Segundo scenario memoria en verde

--- padrino_quickstart/features/step_definitions/memoria_steps.rb
+++ padrino_quickstart/features/step_definitions/memoria_steps.rb
@@ -2,6 +2,15 @@ Given(/^no hice operaciones$/) do
   @calculadora = Calculadora.new
 end
 
-Then(/^la cantidad de operaiones es (\d+)$/) do |arg1|
-  expect(@calculadora.cantidad_operaciones).to eq 0
+Then(/^la cantidad de operaciones es (\d+)$/) do |arg1|
+  expect(@calculadora.cantidad_operaciones).to eq arg1.to_i
+end
+
+Given(/^hago una suma$/) do
+  @calculadora = Calculadora.new
+  @calculadora.sumar(4,5)
+end
+
+Given(/^hago una resta$/) do
+  @calculadora.restar(9,4)
 end
