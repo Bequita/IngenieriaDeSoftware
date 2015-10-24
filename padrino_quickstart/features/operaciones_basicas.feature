e1189bcb5a8e4970573e1b75ae3ff34e4d8e6f88
Bequita <hernan.beca@gmail.com>
2015-10-24 09:58:45 -0300

    Segundo test op.basicas cucumber en verde

--- padrino_quickstart/features/operaciones_basicas.feature
+++ padrino_quickstart/features/operaciones_basicas.feature
@@ -1,22 +1,23 @@
 Feature: Operaciones basicas
 
   Scenario: suma
-    Given voy a la pagina sumar
-    And   un operando es 1
-    And   otro operando es 2
+    Given voy a la pagina operaciones
+    And   un operando suma es 1
+    And   otro operando suma es 2
     When  los sumo
     Then  el resultado es 3
 
-  @wip
   Scenario: resta
-    Given un operando es 5
-    And   otro operando es 4
+    Given voy a la pagina operaciones
+    And   un operando resta es 5
+    And   otro operando resta es 4
     When  los resto
     Then  el resultado es 1
 
   @wip
   Scenario: promedio
-    Given un operando es 10
+    Given voy a la pagina operaciones
+    And   un operando a promediar es 10
     And   otro operando es 4
     When  los promedio
     Then  el resultado es 7
--- padrino_quickstart/features/step_definitions/operaciones_basicas_steps.rb
+++ padrino_quickstart/features/step_definitions/operaciones_basicas_steps.rb
@@ -1,18 +1,31 @@
-Given(/^voy a la pagina sumar$/) do
+Given(/^voy a la pagina operaciones$/) do
   visit 'operaciones'
 end
 
-Given(/^un operando es (\d+)$/) do |arg1|
+Given(/^un operando suma es (\d+)$/) do |arg1|
   fill_in 'op1_suma', with: arg1
 end
 
-Given(/^otro operando es (\d+)$/) do |arg1|
+Given(/^otro operando suma es (\d+)$/) do |arg1|
   fill_in 'op2_suma', with: arg1
+end
+
+Given(/^un operando resta es (\d+)$/) do |arg1|
+  fill_in 'op1_resta', with: arg1
+end
+
+Given(/^otro operando resta es (\d+)$/) do |arg1|
+  fill_in 'op2_resta', with: arg1
 end
 
 When(/^los sumo$/) do
   click_button('sumar')
 end
+
+When(/^los resto$/) do
+  click_button('restar')
+end
+
 
 Then(/^el resultado es (\d+)$/) do |arg1|
   expect(page).to have_content("El resultado de la operacion es: #{arg1}")
